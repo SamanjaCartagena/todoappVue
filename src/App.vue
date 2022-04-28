ecd98f72f79506b876c8332c4eb2514d59f09458
SamanjaCartagena <cart.samanja09@gmail.com>
2022-04-28 16:34:14 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+   <todo-app></todo-app>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoApp from './components/TodoApp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoApp
   }
 }
 </script>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+   name:'ToDoApp',
+   props:{
+       msg:String
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
